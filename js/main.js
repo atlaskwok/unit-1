c5955e0c0098435688f94ca36de55bdfd31d466a
Atlas <chenxiao.atlas.guo@gmail.com>
2019-02-13 22:57:47 -0600

    Module 3 activity completed

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@
 /* Javascript by Chenxiao (Atlas) Guo, 2019 */
 
 //initialize function called when the script loads
-function initialize() {
+function initialize()
+{
 	cities();
+	jQueryAjax(); // Lesson Activities
+	debugAjax(); // On Your Own
 };
 
 //function to create a table with cities and their populations
-function cities() {
+function cities()
+{
 	//define two arrays for cities and population
 	var cityPop = [{
 			city: 'Madison, WI',
@@ -54,10 +58,11 @@ function cities() {
 };
 
 // add a new coloumn of city size
-function addColumns(cityPop) {
+function addColumns(cityPop)
+{
 
-	$('tr').each(function (i) {
-
+	$('tr').each(function (i)
+	{
 		//add header
 		if (i == 0) {
 			$(this).append('<th>City Size</th>');
@@ -78,10 +83,11 @@ function addColumns(cityPop) {
 };
 
 // add the click and mouseover event
-function addEvents() {
+function addEvents()
+{
 	// listen the mouseover event
-	$('table').mouseover(function () {
-
+	$('table').mouseover(function ()
+	{
 		var color = "rgb(";
 		// generate random value for each band
 		for (var i = 0; i < 3; i++) {
@@ -99,13 +105,40 @@ function addEvents() {
 		}
 	});
 
-	function clickme()
+function clickme()
 	{
 		alert('Hey, you clicked me!');
 	};
 
 	// listen the click event
 	$('table').on('click', clickme);
+};
+
+//define AJAX function
+function jQueryAjax(){
+    //basic jQuery ajax method
+    $.getJSON("data/MegaCities.geojson", callback);
+};
+
+// The Ajax function after debug
+function debugAjax()
+{
+	$.ajax("data/MegaCities.geojson", {
+		dataType: "json",
+		success: debugCallback // after success, simply call the callback function
+	});
+};
+
+//define callback function
+function callback(response, status, jqXHRobject){
+    //tasks using the data go here
+    console.log(response);
+};
+
+// the callback function after debug
+function debugCallback(response){
+	// append the response in string type after the <br> element
+	$(mydiv).append('<br>GeoJSON data:</br>' + JSON.stringify(response));
 };
 
 //call the initialize function when the document has loaded
